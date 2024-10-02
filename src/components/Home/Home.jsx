686ca5b5c64dccb78b8bbf07f1cbc4c6a5dab27a
Armin <armin.ts2000@outlook.com>
2024-10-02 23:18:51 +0330

    update home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,20 @@
-
+import img from "./firstpic3.jpg"
+import ix from "./bmwix.jpg"
 
 function Home() {
   return (
-    <section className="flex w-full bg-black h-96">
-      <div className="bg-red-600 w-5/12 flex items-center justify-center">
-      <span className="bg-blue-400 text-5xl font-semibold text-white">Learn everything <br /> about BMW here</span>
+    <section className="flex flex-wrap w-full  bg-black h-96">
+      <div className=" w-5/12 flex items-center justify-center h-full">
+        <span className="text-5xl font-semibold text-white">Learn everything <br /> about BMW here</span>
       </div>
 
 
-      <div className="bg-green-600 w-7/12">r</div>
+      <div className="w-7/12 flex justify-evenly items-center h-full">
+        <img className="h-full rounded-md ms-12" src={img} alt="" />
+      </div>
+
+      <div className="w-6/12 "><img src={ix} alt="" /></div>
+      <div className="w-6/12 flex justify-center items-center"><p className="w-10/12 text-4xl font-semibold"> Enjoy nature consciously – sustainable travel in the BMW iX </p></div>
     </section>
   )
 }
