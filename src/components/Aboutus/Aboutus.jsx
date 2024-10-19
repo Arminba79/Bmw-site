60c51db9cbf4179a9a6e78276a739db70212fa59
Armin <armin.ts2000@outlook.com>
2024-10-20 00:06:41 +0330

    update home page

--- src/components/Aboutus/Aboutus.jsx
+++ src/components/Aboutus/Aboutus.jsx
@@ -1,11 +1,24 @@
-
+import  { useEffect } from 'react';
+import anime from 'animejs/lib/anime.es.js';
 
 function Aboutus() {
-    return (
-        <section className="h-96 bg-yellow-600 w-52 mt-16">
-            1
-        </section>
-    )
+  useEffect(() => {
+    anime({
+      targets: '.anime-target',
+      translateX: 150,
+      
+      
+      duration: 1200
+    });
+  }, []);
+
+  return (
+    <section className="h-96 anime-target bg-yellow-600 w-52 mt-16">
+      <div className=" py-56 text-black text-5xl">
+        محتوا
+      </div>
+    </section>
+  );
 }
 
-export default Aboutus
+export default Aboutus;
