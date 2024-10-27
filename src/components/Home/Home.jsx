41c60114c93e2771b6dfda70be490b69ac1047cf
Armin <armin.ts2000@outlook.com>
2024-10-27 20:37:09 +0330

    responsive updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -38,14 +38,35 @@ function Home() {
 
 
   return (
-    <div>
+    <div >
+      <div className="container w-screen my-16 flex flex-col xl:hidden">
+        <div className="flex flex-col bg-black">
 
-      <div className=" w-full bg-black">
+          <div className="flex flex-col text-3xl font-semibold py-10 ">
+            <span>Power and Beauty, Together</span>
+            <span>Advanced Technology for Your Comfort</span>
+          </div>
+
+          <div>
+            1
+          </div>
+
+          <div>
+
+          </div>
+
+        </div>
+        <div className="bg-blue-500">2</div>
+      </div>
+
+
+
+      <div className="container-fluid w-full bg-black hidden xl:flex " >
 
         <ReactFullpage
           debug
 
-          navigation={true}
+          navigation={false}
 
           easingcss3="cubic-bezier(0.175, 0.885, 0.320, 1.275)"
           licenseKey="xxxxxxxxxxxxxxxxxxxxxxxxx"
@@ -58,9 +79,9 @@ function Home() {
               {/*Slide 1 */}
 
 
-              <div className="section relative bg-cover opacity-80" style={{backgroundImage: `url("https://www.wsupercars.com/wallpapers-wide/BMW/2023-BMW-M3-Competition-Touring-005-1440w.jpg")`}}>
+              <div className="section bg-cover relative opacity-80  " style={{ backgroundImage: `url("https://www.wsupercars.com/wallpapers-wide/BMW/2023-BMW-M3-Competition-Touring-005-1440w.jpg")` }}>
 
-                <div className="absolute top-28 flex flex-col w-full items-start text-white text-5xl font-bold  ">
+                <div className="absolute top-28 flex-col w-full items-start text-white text-5xl font-bold  flex ">
                   <span className="animesp1">Power and Beauty, Together</span>
                   <span className="animesp2">Advanced Technology for Your Comfort</span>
                 </div>
@@ -73,18 +94,18 @@ function Home() {
               <div className="section flex ">
                 <div className="flex flex-wrap justify-center items-center ">
                   <div className=" w-7/12 flex items-center h-full">
-                    <div className="w-full flex justify-center h-96 bg-cover opacity-30 hover:opacity-70 transition rounded-lg" style={{backgroundImage: `url(${ImageSlide2})`}}>
+                    <div className="w-full flex justify-center h-96 bg-cover opacity-30 hover:opacity-70 transition rounded-lg" style={{ backgroundImage: `url(${ImageSlide2})` }}>
                       <span className="text-3xl mt-5"> The BMW journey to the mobility of the future </span>
                     </div>
                   </div>
 
                   <div className="w-5/12 flex flex-col justify-evenly h-max ">
-                    <div style={{backgroundImage: `url("https://images.unsplash.com/photo-1665950798334-6251a5cdb1e9?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`}} 
-                     className="rounded-lg  w-full h-80 opacity-30 hover:opacity-70 transition bg-cover flex justify-center">
+                    <div style={{ backgroundImage: `url("https://images.unsplash.com/photo-1665950798334-6251a5cdb1e9?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")` }}
+                      className="rounded-lg  w-full h-80 opacity-30 hover:opacity-70 transition bg-cover flex justify-center">
                       <span className="text-4xl mt-14 text-white">You are safe</span>
-                      </div>
-                    <div className="rounded-lg flex justify-center bg-green-400 w-full h-80 bg-cover opacity-30 hover:opacity-70 transition" style={{backgroundImage:`url("https://www.ezoo.uk/wp-content/uploads/2022/05/Charging.jpg")`}}>
-                    <span className="text-4xl mt-5 text-white">We love Our World</span>
+                    </div>
+                    <div className="rounded-lg flex justify-center bg-green-400 w-full h-80 bg-cover opacity-30 hover:opacity-70 transition" style={{ backgroundImage: `url("https://www.ezoo.uk/wp-content/uploads/2022/05/Charging.jpg")` }}>
+                      <span className="text-4xl mt-5 text-white">We love Our World</span>
                     </div>
                   </div>
 
